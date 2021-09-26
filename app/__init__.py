6c080f5421b7895c144ac365d0acda9464ad7e60
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-26 12:47:36 +0300

    Added the application factory and registered the required blueprints

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+
+def create_app(config_name):
+    """
+    app factory function
+    :param config_name:
+    :return: app
+    """
+    app = Flask(__name__)
+    from app.main import main as main_bp
+    from app.auth import auth
+    # initialize bootstrap
+    bootstrap.init_app(app)
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth, url_prefix='/auth')
+
+    return app
