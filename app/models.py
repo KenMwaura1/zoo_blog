35187c813951bc014e243e2d681761fb6a908d1c
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-26 23:19:33 +0300

    Updated the Blog, Comment and Subscriber models

--- app/models.py
+++ app/models.py
@@ -35,3 +35,76 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'User: {self.username}'
+
+
+@dataclass
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment = db.relationship('UserComment', backref='blog', lazy='dynamic')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_blog(id):
+        return db.session.query(Blog).filter(Blog.id == id).first()
+
+    @staticmethod
+    def get_all_blogs():
+        return db.session.query(Blog).order_by(db.desc(Blog.date_posted))
+
+    def __repr__(self):
+        return f'Blog: {self.title}'
+
+
+@dataclass()
+class UserComment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    comment = db.Column(db.String)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow())
+    blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_comment(id):
+        return db.session.query(UserComment).filter(id=id)
+
+    @staticmethod
+    def get_all_comments():
+        return db.session.query(UserComment).order_by(db.desc(UserComment.date_posted))
+
+    def __repr__(self):
+        return f'Comment: {self.comment}'
+
+
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber: {self.email}'
