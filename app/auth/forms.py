70371a2c127df321e961b9f6ce3efbeb9acc4ba6
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-27 05:32:03 +0300

    Updated auth forms and templates

---
+++ app/auth/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import Required,Email,EqualTo, Length
+from wtforms import ValidationError
+from ..models import User
+from ..commands import db
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
+class RegForm(FlaskForm):
+    username = StringField('Enter Your Username', validators=[Required(), Length(min=4, max=20)])
+    email = StringField('Email Address', validators=[Required(),Email()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if db.session.query(User.email).filter_by(email = data_field.data).first():
+            raise ValidationError(message="The Email is already in use!")
+    
+    def validate_username(self, data_field):
+        if db.session.query(User.username).filter_by(username = data_field.data).first():
+            raise ValidationError(message="The Username is already in use!")
