2fa4d2ecf054f077acc7000953d920a746a67a20
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-27 16:56:38 +0300

    Updated the app config

--- config.py
+++ config.py
@@ -11,6 +11,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class DevConfig(Config):
